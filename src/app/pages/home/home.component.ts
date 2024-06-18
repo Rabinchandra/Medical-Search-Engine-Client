6f957c0a6888d63946db5afd102cfed4344f71b5
rabinchandra <rabinyen@gmail.com>
2024-06-18 17:37:19 +0530

    Added Service Section in the Homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,5 @@
-import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MedicalSuggestionService } from '../../services/medical-suggestion.service';
-import { IMedicalSearchResponse } from '../../../interface/IMedicalSearchResponse';
-import { RouterLink } from '@angular/router';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
